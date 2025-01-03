2c3c984bba7d97cfec6ead1b15e37487552bafa7
isaiahmartin847 <isaiahmartin847@gmail.com>
2025-01-03 10:48:01 -0700

    Got the middleware working by moving it the the src file

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -3,11 +3,16 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+  console.log("Middleware running smooth");
   const hostname = request.headers.get("host");
+  console.log("Hostname:", hostname);
 
   if (hostname === "test.localhost:3000") {
     return NextResponse.rewrite(new URL("/test", request.url));
   }
-
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: "/:path*",
+};
