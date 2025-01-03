868212e0930cf62a5d38f978efff2a1556063502
isaiahmartin847 <isaiahmartin847@gmail.com>
2025-01-03 16:16:01 -0700

    Added the navbar component

---
+++ client/src/components/landingPage/navbar.tsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+  return (
+    <div className="flex h-[75px] w-full bg-secondaryColor">
+      {/* this is the image div */}
+      <div></div>
+      {/* this is for the brand name */}
+      <div>
+        <h1>Hunt Ref Maps</h1>
+      </div>
+      {/* this is for the nav links */}
+      <div>
+        {/* more */}
+        {/* sign in */}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
