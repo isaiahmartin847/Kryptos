b18e9b01975b3f0c7c4b4aec92992b432845a41e
isaiahmartin847 <isaiahmartin847@gmail.com>
2025-01-03 17:34:42 -0700

    added the clerk to the app side of the application

--- client/src/app/app/layout.tsx
+++ client/src/app/app/layout.tsx
@@ -1,10 +1,13 @@
-import type { Metadata } from "next";
-import "../globals.css";
+"use client";
 
-export const metadata: Metadata = {
-  title: "Reg-Maps",
-  description: "This is an app for getting regulations for hunting",
-};
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import "../globals.css";
 
 export default function RootLayout({
   children,
@@ -12,17 +15,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <head>
-        {/* You can add custom <head> tags here if needed */}
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        />
-      </head>
-      <body className="bg-cover bg-center h-screen bg-primaryColor text-textColor">
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          {/* You can add custom <head> tags here if needed */}
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1"
+          />
+        </head>
+        <body className="bg-cover bg-center h-screen bg-primaryColor text-textColor">
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
