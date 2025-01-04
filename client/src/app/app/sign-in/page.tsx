b930e00c59a74cb1f21e1e768db6402c21991383
isaiahmartin847 <isaiahmartin847@gmail.com>
2025-01-04 10:22:29 -0700

    added the sign in page with the redirection to handle users who are already signed in if not it pushes them to the sign in or sign up page

--- client/src/app/app/sign-in/page.tsx
+++ client/src/app/app/sign-in/page.tsx
@@ -1,9 +1,39 @@
-const SignInPage = () => {
+"use client";
+import { useEffect } from "react";
+import { Clerk } from "@clerk/clerk-js";
+
+const frontendApi = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!frontendApi) {
+  throw new Error(
+    "NEXT_PUBLIC_CLERK_FRONTEND_API is not defined in your environment variables."
+  );
+}
+
+const clerk = new Clerk(frontendApi);
+
+export default function LandingPage() {
+  useEffect(() => {
+    (async () => {
+      try {
+        // Load Clerk
+        await clerk.load();
+
+        const user = await clerk.user;
+        if (user) {
+          window.location.href = "http://app.localhost:3000";
+        } else {
+          clerk.redirectToSignIn();
+        }
+      } catch (error) {
+        console.error("Error loading Clerk:", error);
+      }
+    })();
+  }, []);
+
   return (
-    <div>
-      <div>sign in page</div>
+    <div className="flex items-center justify-center h-screen">
+      <p>Redirecting...</p>
     </div>
   );
-};
-
-export default SignInPage;
+}
