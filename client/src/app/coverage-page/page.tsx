0af24653e7994fd1e4980b45001dcec23601328c
isaiahmartin847 <isaiahmartin847@gmail.com>
2025-01-05 14:34:16 -0700

    added the coverage page and the usa map for checking coverage

---
+++ client/src/app/coverage-page/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useState } from "react";
+import { MapCustomizations, USAMap } from "react-usa-map-fc";
+
+const CoveragePage = () => {
+  const [customize, setCustomize] = useState<MapCustomizations>({
+    MT: { fill: "#1c911b" },
+  });
+
+  // when this is clicked make it open a dialog that has all the info about the coverage
+  const mapHandler = (event: string) => {
+    console.log(event);
+  };
+
+  return (
+    <div className="w-full max-w-screen-lg mx-auto p-4">
+      <USAMap
+        title="USA Map"
+        width={960}
+        height={600}
+        defaultFill="#d3d3d3"
+        customize={customize}
+        onClick={mapHandler}
+      />
+    </div>
+  );
+};
+
+export default CoveragePage;
