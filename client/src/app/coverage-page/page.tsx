6dca5a16c7659d660157247e7cdeed5e96824e5d
isaiahmartin847 <isaiahmartin847@gmail.com>
2025-01-06 15:10:58 -0700

    added the no coverage block

--- client/src/app/coverage-page/page.tsx
+++ client/src/app/coverage-page/page.tsx
@@ -18,11 +18,16 @@ const CoveragePage = () => {
       <Navbar />
       <div className="border-2 mt-8 flex flex-col">
         <h1 className="text-[40px]">Hunt Regs Coverage</h1>
-        <div>
+        <div className="flex space-x-2">
           <div className="flex space-x-2">
             <span className="h-5 w-5 bg-green-600 inline-block"></span>
             <p className="text-xl">Full Coverage</p>
           </div>
+          <div className="flex space-x-2">
+            <span className="h-5 w-5 bg-[#d3d3d3] inline-block"></span>
+            <p className="text-xl">No Coverage</p>
+          </div>
+          <div className="flex space-x-2"></div>
         </div>
       </div>
       <div className="flex justify-center mt-10">
