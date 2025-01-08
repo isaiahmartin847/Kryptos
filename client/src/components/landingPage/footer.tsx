faa3dbbafc4683912c8e7c1fb7344d4a2acd8ce1
isaiahmartin847 <isaiahmartin847@gmail.com>
2025-01-08 14:34:01 -0700

    added the first section to the footer

--- client/src/components/landingPage/footer.tsx
+++ client/src/components/landingPage/footer.tsx
@@ -1,14 +1,24 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const LandingPageFooter = () => {
   return (
-    <footer className="w-full h-[200px] bg-secondaryColor border-t-[1pxs] border-mutedColor">
+    <footer className="flex w-full h-[200px] pt-[39px] pl-[18px] bg-secondaryColor border-t-[1px] border-mutedColor">
       <Image
         src={"/RegMapsLogo.png"}
         alt={"Logo"}
         height={122}
         width={128}
       />
+      {/* company div */}
+      <div>
+        <h1>Company</h1>
+        <div>
+          <Link href={"/about-us"}>About us</Link>
+        </div>
+      </div>
+      {/* polices div */}
+      {/* quick links div */}
     </footer>
   );
 };
