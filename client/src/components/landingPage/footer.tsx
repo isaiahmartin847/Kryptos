100a6c97f6741af56823c7d87e88df5825a936c7
isaiahmartin847 <isaiahmartin847@gmail.com>
2025-01-09 11:13:06 -0700

    added the section to the footer

--- client/src/components/landingPage/footer.tsx
+++ client/src/components/landingPage/footer.tsx
@@ -3,22 +3,32 @@ import Link from "next/link";
 
 const LandingPageFooter = () => {
   return (
-    <footer className="flex w-full h-[200px] pt-[39px] pl-[18px] bg-secondaryColor border-t-[1px] border-mutedColor">
-      <Image
-        src={"/RegMapsLogo.png"}
-        alt={"Logo"}
-        height={122}
-        width={128}
-      />
-      {/* company div */}
-      <div>
-        <h1>Company</h1>
+    <footer className="w-full h-[200px] py-[39px] px-[18px] bg-secondaryColor border-t-[1px] border-mutedColor">
+      <div className="border-2 flex space-x-28">
+        <Image
+          src={"/RegMapsLogo.png"}
+          alt={"Logo"}
+          height={122}
+          width={128}
+        />
+        {/* company div */}
         <div>
-          <Link href={"/about-us"}>About us</Link>
+          <h1 className="text-xl font-semibold">Company</h1>
+          <div className="text-[12px]">
+            <Link href={"/about-us"}>About us</Link>
+          </div>
+        </div>
+        {/* Policies div */}
+        <div>
+          <h1 className="text-xl font-semibold">Policies</h1>
+          <div className="text-[12px]"></div>
+        </div>
+        {/* quick links div */}
+        <div>
+          <h1 className="text-xl font-semibold">Quick links</h1>
+          <div className="text-[12px]"></div>
         </div>
       </div>
-      {/* polices div */}
-      {/* quick links div */}
     </footer>
   );
 };
