c540a9fd676264c4124fd7fdbcfea3468def3df7
isaiahmartin847 <isaiahmartin847@gmail.com>
2025-01-10 22:01:36 -0700

    added the custom error handling to the stripe route

--- backend/internal/stripe/stripe.go
+++ backend/internal/stripe/stripe.go
@@ -15,9 +15,18 @@ type Handler struct {}
 func (h *Handler) Stripe_transaction(c echo.Context) error {
 	transaction := &model.Stripe{}
 
+
+	
+
 	if err := c.Bind(transaction); err != nil {
 		return err
 	}
+
+
+	if transaction.Amount <= 0 {
+		return echo.NewHTTPError(http.StatusBadRequest, "Amount must be a positive number")
+	}
+
 
 
 	return c.JSON(http.StatusCreated, map[string]interface{}{
