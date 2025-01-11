637105265a15c3bacbdf406212b26bfe1e284608
isaiahmartin847 <isaiahmartin847@gmail.com>
2025-01-10 21:22:02 -0700

    added the strip transaction halnder in the stipe package

--- backend/internal/stripe/stripe.go
+++ backend/internal/stripe/stripe.go
@@ -3,23 +3,25 @@ package stripe
 import (
 	"net/http"
 
+	model "github.com/isaiahmartin847/Reg-Maps/internal/models"
+
 	"github.com/labstack/echo/v4"
 )
 
-// Handler is a struct to hold any dependencies for the stripe package.
-type Handler struct{}
+type Handler struct {}
 
-// NewHandler initializes a new Handler for the stripe package.
-func NewHandler() *Handler {
-	return &Handler{}
-}
 
-// Hello is an endpoint handler that responds with "hello".
-func (h *Handler) Hello(c echo.Context) error {
-	return c.String(http.StatusOK, "this is the stripe key")
-}
 
-// RegisterRoutes registers the stripe-related routes with Echo.
-func (h *Handler) RegisterRoutes(e *echo.Group) {
-	e.GET("/hello", h.Hello)
+func (h *Handler) Stripe_transaction(c echo.Context) error {
+	transaction := &model.Strip{}
+
+	if err := c.Bind(transaction); err != nil {
+		return err
+	}
+
+
+	return c.JSON(http.StatusCreated, map[string]interface{}{
+		"amount": transaction.Amount,
+	})
+
 }
