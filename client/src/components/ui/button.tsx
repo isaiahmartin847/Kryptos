bf026b969a7aebb5e9dc7ab23213bc468a60dec5
isaiah <isaiahmartin847@gmail.com>
2025-01-13 12:09:23 -0700

    changed the color of the button in the secondary

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-primaryAccentColor text-primaryColor font-semibold hover:bg-primaryAccentColor/80 rounded-[6px]",
+          "bg-primaryAccentColor text-textColor font-semibold hover:bg-primaryAccentColor/80 rounded-[6px]",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
