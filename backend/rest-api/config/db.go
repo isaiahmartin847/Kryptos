f0cb4f7c04e580d9f82b5192f8cdacf9ba3b8ac7
isaiah <isaiahmartin847@gmail.com>
2025-03-06 16:11:40 -0700

    updated the docker compose and the db connection to fix the connection to the db

--- backend/rest-api/config/db.go
+++ backend/rest-api/config/db.go
@@ -4,26 +4,24 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/isaiahmartin847/Reg-Maps/logger"
-
 	"github.com/isaiahmartin847/Reg-Maps/internal/models"
+	"github.com/isaiahmartin847/Reg-Maps/logger"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func ConnectDatabase() (*gorm.DB, error) {
-
 	host := os.Getenv("POSTGRES_HOST")
 	port := os.Getenv("POSTGRES_PORT")
 	user := os.Getenv("POSTGRES_USER")
-	name := os.Getenv("POSTGRES_NAME")
-	pass := os.Getenv("POSTGRES_PASS")
+	name := os.Getenv("POSTGRES_DB")
+	pass := os.Getenv("POSTGRES_PASSWORD")
 
-	var err error
-	// pass these in as envs
-	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", host, port, user, name, pass)
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s dbname=%s password=%s sslmode=disable connect_timeout=5",
+		host, port, user, name, pass,
+	)
 
-	// Open the database connection
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("error connecting to the database: %w", err)
@@ -38,7 +36,5 @@ func ConnectDatabase() (*gorm.DB, error) {
 	}
 
 	logger.Info("migration success")
-
-	// Return the database connection
 	return db, nil
 }
--- backend/worker-server/internal/db/db.go
+++ backend/worker-server/internal/db/db.go
@@ -15,13 +15,15 @@ func ConnectDatabase() (*gorm.DB, error) {
 	host := os.Getenv("POSTGRES_HOST")
 	port := os.Getenv("POSTGRES_PORT")
 	user := os.Getenv("POSTGRES_USER")
-	name := os.Getenv("POSTGRES_NAME")
-	pass := os.Getenv("POSTGRES_PASS")
+	name := os.Getenv("POSTGRES_DB")
+	pass := os.Getenv("POSTGRES_PASSWORD")
 
 	var err error
 	// pass these in as envs
-	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", host, port, user, name, pass)
-
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s dbname=%s password=%s sslmode=disable connect_timeout=5",
+		host, port, user, name, pass,
+	)
 	// Open the database connection
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
