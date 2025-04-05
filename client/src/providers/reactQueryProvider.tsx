02936819551a971aa5381eb0d9a514192fdbfc2b
isaiah <isaiahmartin847@gmail.com>
2025-04-05 11:01:23 -0600

    added a testing button for testig if this bug is because react query or not

--- client/src/providers/reactQueryProvider.tsx
+++ client/src/providers/reactQueryProvider.tsx
@@ -1,28 +1,14 @@
 "use client";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
 
-export function ReactQueryProvider({
+const queryClient = new QueryClient();
+
+export const ReactQueryProvider = ({
   children,
 }: {
   children: React.ReactNode;
-}) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 1000 * 30, // 30 seconds
-            gcTime: 1000 * 60 * 5, // 5 minutes
-            retry: 3,
-            refetchOnWindowFocus: true,
-          },
-        },
-      }),
-  );
-
+}) => {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
-}
+};
